174904d5a7b38b706539f40bb7cc7cd3e6527d13
Parth Kolekar <parth.kolekar@students.iiit.ac.in>
2015-12-01 18:34:39 +0530

    Updated URL

--- kissanime.js
+++ kissanime.js
@@ -7,35 +7,35 @@ var episodeLinks = $('table.listing a').map(
             }
         });
 $.ajaxSetup({async:false});
-$.getScript("http://kissanime.com/Scripts/asp.js");
+$.getScript(URL+"/Scripts/asp.js");
 var long_url; 
 var videoQuality = '1280x720.mp4'
 var i; 
 var long_test = "";
 for (i = episodeLinks.length - 1 ; i >= 0; i--) {
-	jQuery.ajax({
+    jQuery.ajax({
          url:    URL + episodeLinks[i]['link'], 
          success: function(result) {
                     var $result = eval($(result));
-					var stringStart = result.search("var wra"); 
-					var stringEnd = result.search("document.write"); 
-					var javascriptToExecute = result.substring(stringStart, stringEnd);
-					eval(javascriptToExecute);
-					
-					$("body").append('<div id="episode' + i + '" style="display: none;"></div>')
-					$('#episode' + i).append(wra); 
-					
-					var downloadQualityOptions = $('#episode' + i + ' a').map(function(i,el) { return $(el); });
-					var j; 
-					for(j = 0; j < downloadQualityOptions.length; j++) {
-						if(videoQuality === downloadQualityOptions[j].html()) {
-							long_url = downloadQualityOptions[j].attr('href');
-							long_test += "curl -C- -L -o \"" + episodeLinks[i]['name'] + "\".mp4 \"" + long_url + "\" && ";
-						}
-					}
+                    var stringStart = result.search("var wra"); 
+                    var stringEnd = result.search("document.write"); 
+                    var javascriptToExecute = result.substring(stringStart, stringEnd);
+                    eval(javascriptToExecute);
+                    
+                    $("body").append('<div id="episode' + i + '" style="display: none;"></div>')
+                    $('#episode' + i).append(wra); 
+                    
+                    var downloadQualityOptions = $('#episode' + i + ' a').map(function(i,el) { return $(el); });
+                    var j; 
+                    for(j = 0; j < downloadQualityOptions.length; j++) {
+                        if(videoQuality === downloadQualityOptions[j].html()) {
+                            long_url = downloadQualityOptions[j].attr('href');
+                            long_test += "curl -C- -L -o \"" + episodeLinks[i]['name'] + "\".mp4 \"" + long_url + "\" && ";
+                        }
+                    }
                   },
          async:   false, 
-		 script:  true
+         script:  true
     });       
 }
 
